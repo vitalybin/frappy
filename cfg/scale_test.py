e9af056e423117b3c19691642631cd2afe2ee184
vitalybin <104634000+vitalybin@users.noreply.github.com>
2025-01-10 01:39:44 +0100

    Update scale_test.py
    name lower case

--- cfg/scale_test.py
+++ cfg/scale_test.py
@@ -1,4 +1,4 @@
-Node('scaleint_HZB',  # a globally unique identification
+Node('scaleint_hzb',  # a globally unique identification
      'scaledInt Test sec-Node',  # describes the node
       'tcp://10771',
       implementor = 'Peter Wegmann')  # you might choose any port number > 1024
